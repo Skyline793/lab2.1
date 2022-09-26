f3d436c4598e840ba6f2e9e937cb316e05489112
Ivan Tyukavkin <ivan.ru614@gmail.com>
2022-09-26 21:04:50 +0700

    добавил структуру "параллелограмм" и функции по работе с ней

--- lab2.1.cpp
+++ lab2.1.cpp
@@ -145,6 +145,42 @@ double TriangleCalcPerimeter(Triangle t) {
 }
 
 
+typedef struct {
+    Vector a, b;
+} Parallelogram;
+
+void ParallelogramInit(Parallelogram* pr, int x1, int y1, int x2, int y2) {
+    VectorInit(&pr->a, x1, y1);
+    VectorInit(&pr->b, x2, y2);
+}
+
+void ReadParallelogram(Parallelogram* pr) {
+    int x1, y1, x2, y2;
+    printf("Введите координаты первого из векторов, на которых построен параллелограм (x1, y1): ");
+    scanf("%d%d", &x1, &y1);
+    printf("Введите координаты второго из векторов, на которых построен параллелограм (x1, y1): ");
+    scanf("%d%d", &x2, &y2);
+    ParallelogramInit(pr, x1, y1, x2, y2);
+}
+
+void ParallelogramDisplay(Parallelogram pr) {
+    puts("Параллелограм, построенный на векторах a и b:");
+    VectorDisplay(pr.a);
+    VectorDisplay(pr.b);
+}
+
+double ParallelogramCalcSquare(Parallelogram pr) {
+    double S, len_a, len_b, prod, cosinus, sinus;
+    len_a = VectCalcLen(pr.a);
+    len_b = VectCalcLen(pr.b);
+    prod = CalcScalarProd(pr.a, pr.b);
+    cosinus = prod / (len_a * len_b);
+    sinus = sqrt(1 - cosinus * cosinus);
+    S = len_a * len_b * sinus;
+    return S;
+}
+
+
 int main()
 {
     SetConsoleCP(1251);
