f6fa688a9eb4fd247fcdb8a317d09a376fd39ca1
Ivan Tyukavkin <ivan.ru614@gmail.com>
2022-09-26 21:00:05 +0700

    Добавил структуру "вектор" и функции по работе с ней

--- lab2.1.cpp
+++ lab2.1.cpp
@@ -9,6 +9,38 @@ typedef struct
     int X, Y;
 } TPoint;
 
+typedef struct {
+    TPoint coord;
+} Vector;
+
+void VectorInit(Vector* v, int x, int y) {
+    v->coord.X = x;
+    v->coord.Y = y;
+}
+
+void VectorRead(Vector* v) {
+    printf("Введите координаты вектора (x, y): ");
+    scanf("%d%d", &v->coord.X, &v->coord.Y);
+
+}
+
+void VectorDisplay(Vector v) {
+    printf("Вектор с координатами: (%d,%d)\n", v.coord.X, v.coord.Y);
+}
+
+
+double VectCalcLen(Vector v) {
+    double len;
+    len = sqrt(v.coord.X * v.coord.X + v.coord.Y * v.coord.Y);
+    return len;
+}
+
+double CalcScalarProd(Vector v1, Vector v2) {
+    double prod;
+    prod = v1.coord.X * v2.coord.X + v1.coord.Y * v2.coord.Y;
+    return prod;
+}
+
 int main()
 {
     SetConsoleCP(1251);
